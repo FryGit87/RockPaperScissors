9cffac3e606997726a0863cd44e2077039c86a80
Kym Reilly <kymreilly@hotmail.com>
2022-03-21 10:56:47 +1030

    more JS added, more JS needed

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
@@ -14,23 +14,49 @@ function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() *3);
     return choices[randomNumber];
 }
+
+function convertToWord(letter){
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors";
+}
+
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span = computerScore;
+    result_p.innerHTML = "WINNER!!!: " + convertToWord(userChoice) + " destroys " + convertToWord(computerChoice) + "!"
+
+}
+
+function lose() {
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    userScore_span = userScore;}
+
+function draw() {
+    
+}
+
+
+
 function game (userChoice) {
     const computerChoice = getComputerChoice();
     switch (userChoice + computerChoice) {
         case "rs":
         case "pr":
         case "sp":
-            //  user win
+            win(userChoice, computerChoice);
             break;
         case "rp":
         case "ps":
         case "sr":
-            // user lose
+            lose(userChoice, computerChoice);
             break;
         case "rr":
         case "pp":
         case "ss":
-            // draw
+            draw(userChoice, computerChoice);
             break;
     }
 
